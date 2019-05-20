738c9eebc5c999a16ce14ae9b301ea186fc7ac34
gardnern <48269958+gardnern@users.noreply.github.com>
2019-05-20 11:17:56 -0600

    switch case changes

--- liri.js
+++ liri.js
@@ -3,74 +3,134 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
-console.log(process.argv)
-var command = process.argv[2];
-var otherArgs = process.argv.slice(3).join(' ');
-console.log(command)
-console.log(otherArgs)
-var functionMap = {
-    'spotify-this-song':spotifySong,
-    'concert-this':findEvents,
-    'movie-this' :findMovie,
-    'do-what-it-says' :dowhatever
-}
+var fs = require("fs");
 
-// if(functionMap[process.argv[2]]){
-//   functionMap[process.argv[2]](otherArgs)
-// }
-// else {
-//   console.log('That command is not supported')
-// }
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+var userCommand = function(caseData, functionData) {
+  switch (caseData) {
+
+    case "concert-this":
+      findConcert(functionData);
+    break;
+
+    case "spotify-this-song":
+      spotifyAPI(functionData);
+      break;
+  
+    case "movie-this":
+    omdbAPI(functionData);
+    break;
+
+    case "do-what-it-says":
+    doWhatItSays();
+    break;
+    default:
+    console.log("Sure, whatever you say!");
   }
- 
-console.log(data); 
-});
+};
 
 
-if(functionMap[process.argv[2]]){
-  functionMap[process.argv[2]](otherArgs)
-}
-else {
-  console.log('That command is not supported')
-}
 
-function spotifySong(){
-  //pretend gets real songs
-  console.log('My spotify song');
-}
+var findConcert = function(artists) {
 
-function findEvents(artist){
-
-axios.get("https://rest.bandsintown.com/artists/" + (artist || 'Rolling Stones') +"/events?app_id=847492e9-710f-445d-81dc-db9b772ae682")
+axios.get("https://rest.bandsintown.com/artists/" + (artists || 'Rolling Stones') +"/events?app_id=847492e9-710f-445d-81dc-db9b772ae682")
 .then(
-function(response) {
-  
-  console.log(response.data);
-})
-.catch(
-function(error) {
-  console.log(error)
-}
-);
-}
-
-var dowhatever;
-
-function findMovie(movieName, dowhatever) {
-  
-axios.get("http://www.omdbapi.com/?t=" + (movieName || 'Mr. Nobody' ||dowhatever) + "&y=&plot=short&apikey=trilogy")
-.then(
-console.log(queryURL),
   function(response) {
-
-    console.log("Release Year: " + response.data.Title);
+    console.log("Name of the venue:  " + venue.data);
+    console.log("Venue location:  " + location.data);
+    console.log("Date of the Event:  " + jsonData.moment(MMDDYYYY));
+    console.log(findConcert);
   })
   .catch(
-function (error) {
- console.log(error);
+  function(error) {
+    console.log(error)
   }
 );
 }
+
+ 
+
+var spotifyAPI = function(songName) {
+  if (songName === undefined) {
+    songName = "The Sign";
+  }
+  spotify.search(
+    {
+      type: "track",
+      query: songName,
+      limit: 10
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+      var songs = data.tracks.items;
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("Artist name: " + songs[i].artists[0].name);
+        console.log("Song title: " + songs[i].name);
+        console.log("Track number: " + songs[i].track_number);
+        console.log("Album: " + songs[i].album.name);
+        console.log("Release date: " + songs[i].album.release_date);
+        console.log("Album type: " + songs[i].album.album_type);
+        console.log("Preview song: " + songs[i].preview_url);
+        console.log("----------------------------------------------------");
+      }
+    }
+  );
+};
+
+
+var omdbAPI = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Mr. Nobody";
+  }
+  var url =
+  "http://www.omdbapi.com/?t=" +
+  movieName +
+  "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var jsonData = JSON.parse(body);
+      console.log("Movie Title: " + jsonData.Title);
+      console.log("Year Release: " + jsonData.Year);
+      console.log("IMDB Rating: " + jsonData.imdbRating);
+      console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
+      
+    }
+  });
+};
+
+
+
+var doWhatItSays = function () {
+  fs.readFile('random.txt', 'utf8', function(err, data){
+    console.log(data);
+    var dataArr = data.split(' ');
+    if (dataArr.length ===  2) {
+      userCommand(dataArr[0], dataArr[1]);
+    } else if (dataArr.length === 1) {
+      userCommand(dataArr[3]);
+    }
+  });
+};
+
+userCommand = process.argv[2]; 
+  var userInput = process.argv.slice(3).join(' ');
+  console.log(userInput);
+
+
+fs.writeFile("log.txt", userCommand, function(err) {
+  console.log("****************************Request********************************");
+
+   if (err) {
+    return console.log(err);
+  }
+
+  console.log("log.txt was updated!");
+
+});
